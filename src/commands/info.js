521a53d982fd12adeb8eab8ceac7112876d50502
Jonas Sterud <jonas.sterud@gmail.com>
2020-01-31 23:07:55 +0100

    formatting

--- src/commands/info.js
+++ src/commands/info.js
@@ -9,9 +9,9 @@ module.exports = {
         const member = message.mentions.members.first();
         let embed = new Discord.RichEmbed();
 
-        embed.setColor('#ff0000');
         embed.setTitle(member.user.tag);
         embed.setThumbnail(member.user.avatarURL);
+        embed.setColor('#ff0000');
         embed.addField('Identification:', member.user.id);
         embed.addField('Account created:', member.user.createdAt.toDateString());
         
