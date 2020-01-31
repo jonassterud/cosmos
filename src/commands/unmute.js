107271a6700e73542e2722593ba8cb279767d854
Jonas Sterud <jonas.sterud@gmail.com>
2020-01-31 22:42:06 +0100

    unmute command

---
+++ src/commands/unmute.js
@@ -0,0 +1,17 @@
+module.exports = {
+	name: 'unmute',
+    description: '\:loud_sound: Mute a user!',
+    args: true,
+    usage: '<user>',
+	execute(message, args) {
+        const member = message.mentions.members.first();
+        const reason = args.splice(1).join(' ');
+
+        if(message.member.hasPermission('ADMINISTRATOR')) {
+            member.setMute(false, reason);
+            message.channel.send("\:loud_sound: Unmuted <@" + member.user.id + ">" + (reason.length > 0 ? " for " + reason : "") + "!");
+        } else {
+            message.channel.send("\:no_entry: You can't unmute that person, <@" + message.author.id + ">!");
+        }
+	}
+};
