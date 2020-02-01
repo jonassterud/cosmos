152fdd76d3054ed9a57fab3c09b62fc7ff4f0844
Jonas Sterud <jonas.sterud@gmail.com>
2020-02-01 23:24:57 +0100

    gif command

---
+++ src/commands/gif.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js'); // Node.js package - Discord API
+const secret = require('../config.json'); // Secret data
+const request = require('request'); // Node.js package - request webiste data
+
+module.exports = {
+	name: 'gif',
+    description: '\:frame_photo: Search for a GIF!',
+    args: true,
+    usage: '<search>',
+	execute(message, args) {
+        request({
+            url: 'http://api.giphy.com/v1/gifs/search?api_key=' + secret.giphy + '&q=' + args.join('+'),
+            json: true
+        },
+        function(e, r, body) {
+            // Errors:
+            if(e) return message.channel.send("\:no_entry: Wasn't able to retrieve any GIFs, <@" + message.author.id + ">!");
+            if(!body.data.length) return message.channel.send("\:no_entry: No GIFs were found, <@" + message.author.id + ">!");
+            
+            // Variables:
+            let embed = new Discord.RichEmbed();
+            const gif = body.data[Math.floor(Math.random() * body.data.length)];
+            
+            // Edit embed:
+            embed.setColor('#ff0000');
+            embed.image = {url: gif.images.downsized.url.replace('media1', 'i')};
+            
+            // Send embed:
+            return message.channel.send(embed);
+        });
+	}
+};
