2f5e3765730b0c4d327861e76c7e1a15b120f81b
Jonas Sterud <jonas.sterud@gmail.com>
2020-02-01 20:08:56 +0100

    more comments

--- src/commands/help.js
+++ src/commands/help.js
@@ -7,21 +7,28 @@ module.exports = {
     args: false,
     usage: '',
 	execute(message, args) {
+        // Create embed:
         let embed = new Discord.RichEmbed();
-        embed.setColor('#ff0000');
 
         if(args.length) {
+            // Get command:
             const command = client.commands.get(args[0]);
+            if(command === undefined) return message.channel.send("\:no_entry: Command wasn't found, <@" + message.author.id + ">!");
+            
+            // Edit embed:
             embed.setTitle(command.name);
             embed.addField('Description', command.description + (command.usage ? '\n' + command.usage : ''));
             embed.addField('Format', '`' + secret.prefix + command.name + (command.usage ? ' ' + command.usage : '') + '`');
         } else {
+            // Edit embed:
             embed.setTitle('\:closed_book: Command list');
             client.commands.tap(command => {
                 embed.addField(secret.prefix + command.name + ' ' + (command.usage ? command.usage : ''), command.description);
             });  
         }
-
+        
+        // Send embed:
+        embed.setColor('#ff0000');
         message.channel.send(embed);        
 	}
 };
