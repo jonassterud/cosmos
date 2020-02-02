afa4767a6a68f8655ebb84cfc730c96f5ef67a0e
Jonas Sterud <jonas.sterud@gmail.com>
2020-02-02 17:04:13 +0100

    missing symbol

--- src/commands/balance.js
+++ src/commands/balance.js
@@ -5,12 +5,13 @@ module.exports = {
     description: '\:moneybag: balance!',
     args: false,
     usage: '<user>',
-	execute(message) {
+	execute(message, args) {
         // Variables:
         const user = ((message.mentions.users.first() === undefined) ? message.author : message.mentions.users.first());
-        const credits = JSON.parse(fs.readFileSync('./data.json'))[message.guild.id]['users'][user.id]['credits'];
+        const data = JSON.parse(fs.readFileSync('./data.json'));
+        const credits = data[message.guild.id]['users'][user.id]['credits'];
 
         // Execute:
-        return message.channel.send("<@" + user.id + "> has " + credits + " credit" ((credits > 1) ? "s" : "") + "!");
+        return message.channel.send("<@" + user.id + "> has " + credits + " credit" + ((credits > 1) ? "s" : "") + "!");
 	}
 };
