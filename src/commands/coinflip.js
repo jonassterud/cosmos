25628fb8e031abb6a6c27e9e44efd01d542754b7
Jonas Sterud <jonas.sterud@gmail.com>
2020-02-02 18:22:21 +0100

    added option to coinflip vs user - needs testing

--- src/commands/coinflip.js
+++ src/commands/coinflip.js
@@ -1,14 +1,18 @@
 const fs = require('fs'); // Node.js package - file system
+const secret = require('../config.json'); // Secret data
 
+//! NEEDS REFORMATTING
 module.exports = {
 	name: 'coinflip',
     description: '\:dollar: Make a coinflip bet!',
-    args: false,
-    usage: '<user>',
+    args: true,
+    usage: '<amount> <user>',
 	execute(message, args) {
         // Variables:
-        const data = JSON.parse(fs.readFileSync('./data.json'));
+        const member = message.mentions.members.first();
         const amount = parseInt(args[0]);
+        const won = Math.floor(Math.random() * 2);
+        let data = JSON.parse(fs.readFileSync('./data.json'));
 
         // Check for errors:
         if(isNaN(amount)) return message.channel.send("\:no_entry: You need to specify the amount of credits you want to bet, <@" + message.author.id + ">!");
@@ -16,9 +20,28 @@ module.exports = {
         if(amount > data[message.guild.id]['users'][message.author.id]['credits']) return message.channel.send("\:moneybag: You don't have enough credits, <@" + message.author.id + ">!");       
 
         // Execute:
-        const won = Math.floor(Math.random() * 2);
-        data[message.guild.id]['users'][message.author.id]['credits'] += (won ? amount : -amount);
-        message.channel.send((won ? "\:money_with_wings:" : "\:weary:") + " You " + (won ? "won " : "lost ") + amount + " credits, <@" + message.author.id + ">!");
+        if(member === undefined) {
+            // Against computer:
+            data[message.guild.id]['users'][message.author.id]['credits'] += (won ? amount : -amount);
+            message.channel.send((won ? "\:money_with_wings:" : "\:weary:") + " You " + (won ? "won " : "lost ") + amount + " credits, <@" + message.author.id + ">!");
+        } else {
+            // Against opponent:
+            const bet = data[message.guild.id]['users'][member.id]['coinflip']; 
+            if(bet.opponent === message.author.id && bet.amount === amount) { // Check if bet already exists
+                // Execute bet:
+                data[message.guild.id]['users'][message.author.id]['credits'] += (won ? amount : -amount);
+                data[message.guild.id]['users'][member.id]['credits'] += (won ? -amount : amount);
+                message.channel.send("\:money_with_wings: You won " + amount + " credits, <@" + (won ? message.author.id : member.id) + ">!");
+
+                // Delete bet:
+                data[message.guild.id]['users'][message.author.id]['coinflip']['amount'] = 0;
+                data[message.guild.id]['users'][message.author.id]['coinflip']['opponent'] = '';
+            } else { // Create bet
+                data[message.guild.id]['users'][message.author.id]['coinflip']['amount'] = amount;
+                data[message.guild.id]['users'][message.author.id]['coinflip']['opponent'] = member.id;
+                message.channel.send("\:white_check_mark: Successfully created bet, <@" + message.author.id + ">!\nTo join <@" + member.id + ">, type: *" + secret.prefix + "coinflip " + amount + " <@" + message.author.id + ">*");
+            }
+        }
 
         // Write data:
         return fs.writeFileSync('./data.json', JSON.stringify(data));
