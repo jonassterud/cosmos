94eb9b0be5a87733c04366e2adc9d972ef294ef7
Jonas Sterud <jonas.sterud@gmail.com>
2020-02-03 22:36:50 +0100

    simplified

--- src/commands/coinflip.js
+++ src/commands/coinflip.js
@@ -19,30 +19,35 @@ module.exports = {
         if(amount > data[message.guild.id]['users'][message.author.id]['credits']) return message.channel.send("\:moneybag: You don't have enough credits, <@" + message.author.id + ">!");       
 
         // Execute:
-        if(member === undefined) {
-            // Against computer:
-            data[message.guild.id]['users'][message.author.id]['credits'] += (won ? amount : -amount);
-            message.channel.send((won ? "\:money_with_wings:" : "\:weary:") + " You " + (won ? "won " : "lost ") + amount + " credits, <@" + message.author.id + ">!");
-        } else {
-            // Against opponent:
-            const bet = data[message.guild.id]['users'][member.id]['coinflip']; 
-            if(bet.opponent === message.author.id && bet.amount === amount) { // Check if bet already exists
-                // Execute bet:
-                data[message.guild.id]['users'][message.author.id]['credits'] += (won ? amount : -amount);
-                data[message.guild.id]['users'][member.id]['credits'] += (won ? -amount : amount);
-                message.channel.send("\:money_with_wings: You won " + amount + " credits, <@" + (won ? message.author.id : member.id) + ">!");
+        data[message.guild.id]['users'][message.author.id]['credits'] += (won ? amount : -amount);
+        if(member) {
+            const bet = data[message.guild.id]['users'][member.user.id]['coinflip'];
+            if(bet.opponent == message.author.id && bet.amount == amount) {
+                // Bet:
+                data[message.guild.id]['users'][member.user.id]['credits'] += (won ? -amount : amount);
 
                 // Delete bet:
-                data[message.guild.id]['users'][message.author.id]['coinflip']['amount'] = 0;
-                data[message.guild.id]['users'][message.author.id]['coinflip']['opponent'] = '';
-            } else { // Create bet
+                data[message.guild.id]['users'][member.user.id]['coinflip']['amount'] = 0;
+                data[message.guild.id]['users'][member.user.id]['coinflip']['opponent'] = "";
+
+                // Respond:
+                message.channel.send("\:money_with_wings: You won " + amount + " credits, <@" + (won ? message.author.id : member.user.id) + ">!");
+                message.channel.send("\:weary: You lost " + amount + " credits, <@" + (won ? member.user.id : message.author.id) + ">!");
+            } else {
+                // Bet:
                 data[message.guild.id]['users'][message.author.id]['coinflip']['amount'] = amount;
-                data[message.guild.id]['users'][message.author.id]['coinflip']['opponent'] = member.id;
-                message.channel.send("\:white_check_mark: Successfully created bet, <@" + message.author.id + ">!\nTo join <@" + member.id + ">, type: *" + secret.prefix + "coinflip " + amount + " <@" + message.author.id + ">*");
+                data[message.guild.id]['users'][message.author.id]['coinflip']['opponent'] = member.user.id;
+                
+                // Respond:
+                message.channel.send("\:white_check_mark: Successfully created bet, <@" + message.author.id + ">!");
+                message.channel.send("To join <@" + member.user.id + ">, type: *" + secret.prefix + "coinflip " + amount + " <@" + message.author.id + ">*");
             }
+        } else {
+            // Respond:
+            message.channel.send((won ? "\:money_with_wings:" : "\:weary:") + " You " + (won ? "won " : "lost ") + amount + " credits, <@" + message.author.id + ">!");
         }
 
         // Write data:
         return fs.writeFileSync('./data.json', JSON.stringify(data));
-	}
+    }
 };
