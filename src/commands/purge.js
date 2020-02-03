38d5e2ad4074ef7589771d0588a5945ad0b0a5b6
Jonas Sterud <jonas.sterud@gmail.com>
2020-02-03 21:47:54 +0100

    fixed regex

--- src/commands/purge.js
+++ src/commands/purge.js
@@ -5,7 +5,7 @@ module.exports = {
     usage: '<amount>',
 	execute(message, args) {
         // Check for errors:
-        if(!/100|^[1-9][0-9]$|^[1-9]$/.test(args[0])) return message.channel.send("\:no_entry: Invalid amount. Make sure the amount is in the range from 1 to 100, <@" + message.author.id + ">!");
+        if(!/^100$|^[1-9][0-9]$|^[0-9]$/m.test(args[0])) return message.channel.send("\:no_entry: Invalid amount. Make sure the amount is in the range from 1 to 100, <@" + message.author.id + ">!");
 
         // Execute:
         message.channel.bulkDelete(parseInt(args[0]));
