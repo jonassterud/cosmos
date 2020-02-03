b724ed9cdd9b827e6a8069e50fd33bb96f2923cb
Jonas Sterud <jonas.sterud@gmail.com>
2020-02-03 22:40:44 +0100

    fixed comment

--- src/commands/coinflip.js
+++ src/commands/coinflip.js
@@ -23,7 +23,7 @@ module.exports = {
         if(member) {
             const bet = data[message.guild.id]['users'][member.user.id]['coinflip'];
             if(bet.opponent == message.author.id && bet.amount == amount) {
-                // Bet:
+                // Execute bet:
                 data[message.guild.id]['users'][member.user.id]['credits'] += (won ? -amount : amount);
 
                 // Delete bet:
@@ -34,7 +34,7 @@ module.exports = {
                 message.channel.send("\:money_with_wings: You won " + amount + " credits, <@" + (won ? message.author.id : member.user.id) + ">!");
                 message.channel.send("\:weary: You lost " + amount + " credits, <@" + (won ? member.user.id : message.author.id) + ">!");
             } else {
-                // Bet:
+                // Create bet:
                 data[message.guild.id]['users'][message.author.id]['coinflip']['amount'] = amount;
                 data[message.guild.id]['users'][message.author.id]['coinflip']['opponent'] = member.user.id;
                 
