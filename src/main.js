fa7dd6d18a5251f1172cd255a4bdd96c5ea2749f
Sebastian Refsnes <sebastian.refsnes@gmail.com>
2020-02-11 18:26:53 +0100

    Attempt at fixing crash whenever someone commits to github

--- src/main.js
+++ src/main.js
@@ -15,16 +15,16 @@ commandFiles.forEach(f => {
 });
 
 // Ready event
-client.once('ready', () => {    
+client.once('ready', () => {
     // Create data:
-    if(!fs.existsSync('./data.json')) fs.writeFileSync('./data.json', '{}');
+    if (!fs.existsSync('./data.json')) fs.writeFileSync('./data.json', '{}');
     let data = JSON.parse(fs.readFileSync('./data.json'));
     client.guilds.tap(guild => {
-        if(!data.hasOwnProperty(guild.id)) data[guild.id] = {};
-        if(!data[guild.id].hasOwnProperty('users')) data[guild.id]['users'] = {};
+        if (!data.hasOwnProperty(guild.id)) data[guild.id] = {};
+        if (!data[guild.id].hasOwnProperty('users')) data[guild.id]['users'] = {};
         guild.members.tap(member => {
-            if(!data[guild.id]['users'].hasOwnProperty(member.id)) data[guild.id]['users'][member.id] = {};
-            if(!data[guild.id]['users'][member.id].hasOwnProperty('credits')) data[guild.id]['users'][member.id]['credits'] = 5000;
+            if (!data[guild.id]['users'].hasOwnProperty(member.id)) data[guild.id]['users'][member.id] = {};
+            if (!data[guild.id]['users'][member.id].hasOwnProperty('credits')) data[guild.id]['users'][member.id]['credits'] = 5000;
         });
     });
 
@@ -33,36 +33,39 @@ client.once('ready', () => {
 
     // Other:
     console.log("Online!");
-    client.user.setActivity(secret.prefix + "help", {type: "LISTENING"});
+    client.user.setActivity(secret.prefix + "help", {
+        type: "LISTENING"
+    });
 });
 
 // Message event
 client.on('message', message => {
     // Add to balance:
-    if(message.guild.id && message.author.id) { // Small fix for weird bug where this event would fire randomly
+    if (message.guild.id && message.author.id) { // Small fix for weird bug where this event would fire randomly
         let data = JSON.parse(fs.readFileSync('./data.json'));
+        if (!data || !message || !message.guild.id || !message.author.id) return;
         data[message.guild.id]['users'][message.author.id]['credits']++;
         fs.writeFileSync('./data.json', JSON.stringify(data));
     }
-    
+
     // Split command and arguments:
-    if(!message.content.startsWith(secret.prefix) || message.author.bot) return;
+    if (!message.content.startsWith(secret.prefix) || message.author.bot) return;
     const args = message.content.slice(secret.prefix.length).split(/ +/);
     const commandName = args.shift().toLowerCase();
-    if(!client.commands.has(commandName)) return;
-    
+    if (!client.commands.has(commandName)) return;
+
     // Retrieve command:
     const command = client.commands.get(commandName);
-    if(command.args && !args.length) {
+    if (command.args && !args.length) {
         let reply = "Missing arguments!";
-        if(command.usage) reply += "\nFormat: `" + secret.prefix + command.name + " " + command.usage + "`";
+        if (command.usage) reply += "\nFormat: `" + secret.prefix + command.name + " " + command.usage + "`";
         message.channel.send(reply);
     }
-    
+
     // Execute command:
     try {
         command.execute(message, args);
-    } catch(e) {
+    } catch (e) {
         console.error("Command execution error:\n" + e);
     }
 });
@@ -70,6 +73,6 @@ client.on('message', message => {
 // Bot login
 try {
     client.login(secret.token);
-} catch(e) {
+} catch (e) {
     console.error("Bot login error:\n" + e);
 }
