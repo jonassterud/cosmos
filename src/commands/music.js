ae8500507e6bd1f52ec89a349991afdf4fe0a67e
Jonas Sterud <jonas.sterud@gmail.com>
2020-02-13 02:14:45 +0100

    formatting

--- src/commands/music.js
+++ src/commands/music.js
@@ -13,10 +13,10 @@ module.exports = {
     execute(message, args) {
         // Variables:
         const voice = message.member.voiceChannel;
-        if (!voice) return message.channel.send("\:no_entry: Please join a voicechannel before excecuting the command");
+        if(!voice) return message.channel.send("\:no_entry: Please join a voicechannel before executing the command, <@" + message.author.id + ">!");
 
-        //Check for skip
-        if (args[0] == "skip") {
+        // Check argument:
+        if(args[0] === 'skip') {
             if (connection != "") {
                 connection.dispatcher.end();
                 return message.channel.send("\:cd: Skipping song...")
@@ -24,45 +24,42 @@ module.exports = {
                 return message.channel.send("No songs in queue " + message.author);
             }
         }
-        if (args[0] == "queue") {
-            if (!queue.length) return message.channel.send("No songs in queue " + message.author);
+        else if(args[0] === 'queue') {
+            if(!queue.length) return message.channel.send("No songs in queue " + message.author);
+            
             let embed = new Discord.RichEmbed()
                 .setTitle("Song queue \:musical_note:")
                 .setColor('#ff0000')
                 .setTimestamp(new Date());
-            const loop = async _ => {
-                for (let i = 0; i < queue.length; i++) {
+            
+            const loop = async() => {
+                for(let i = 0; i < queue.length; i++) {
                     let songUrl = queue[i];
                     await ytdl.getInfo(songUrl).then(info => {
-                        if (i == 0) {
-                            embed.addField("Current song:", " " + info.title);
-                        } else {
-                            embed.addField("Song number " + i + ":", " " + info.title);
-                        }
-                        if (i == queue.length - 1) {
-                            return message.channel.send(embed);
-                        }
+                        if (i == 0) embed.addField("Current song:", " " + info.title);
+                        else embed.addField("Song number " + i + ":", " " + info.title);
+                        if (i == queue.length - 1) return message.channel.send(embed);
                     }).catch();
                 }
             }
+            
             loop();
-
             return;
         }
 
         // Add to queue:
-        //Make sure this is actually a youtube link
         const re = /youtube.com\/watch\?v=[a-zA-Z\d\=\&\-\_]+$/g
-        let ytbUrl = args[0];
+        
+        // Check for errors:
+        if (!re.test(args[0])) return message.channel.send("\:no_entry: Please enter a valid youtube url");
 
-        if (!re.test(ytbUrl)) return message.channel.send("\:no_entry: Please enter a valid youtube url");
-
-        queue.push(ytbUrl);
+        // Add to queue:
+        queue.push(args[0]);
         message.channel.send("\:ok_hand: The video was added to the queue, <@" + message.author.id + ">!");
 
         // Play:
-        if (!playing) {
-            if (connection == "") {
+        if(!playing) {
+            if(connection === '') {
                 voice.join().then(conn => {
                     connection = conn;
                     play();
@@ -73,33 +70,22 @@ module.exports = {
         }
 
         function play() {
-            if (queue.length) {
+            if(queue.length) {
                 // Change state:
                 playing = true;
+                
                 // Print title:
                 ytdl.getInfo(queue[0]).then(info => message.channel.send("\:musical_note: Now playing: " + info.title)).catch();
 
                 // Play song:
-                // Variables:
-
-                let stream = ytdl(queue[0], {
-                    quality: "lowest",
-                    filter: "audioonly"
-                });
-
-                let disp = connection.playStream(stream, {
-                    seek: 0,
-                    volume: 1
-                });
+                let stream = ytdl(queue[0], {quality: "lowest", filter: "audioonly"});
+                let disp = connection.playStream(stream, {seek: 0, volume: 1});
 
                 // Finished song event:
                 disp.on('end', () => {
                     queue.shift();
                     play();
                 });
-
-
-
             } else {
                 playing = false;
                 connection = "";
