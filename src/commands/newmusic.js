2a5c833acd12359a3049cf791def5b810edf9074
jonassterud <jonas.sterud@gmail.com>
2020-04-26 20:30:35 +0200

    Wrong song option and skip amount added

--- src/commands/newmusic.js
+++ src/commands/newmusic.js
@@ -6,7 +6,7 @@ module.exports = {
     name: 'newmusic',
     description: '\:musical_note: Listen to the audio from a YouTube video in your current voice channel!',
     args: true,
-    usage: '<YouTube video or playlist | skip | queue | pause | resume | reset>',
+    usage: '<YouTube video or playlist | skip [amount] | queue | pause | resume | reset>',
     execute(message, args) {
         // Variables:
         const voice = message.member.voiceChannel;
@@ -41,6 +41,13 @@ module.exports = {
                 if(!queue[message.guild.id] || !queue[message.guild.id].connection) return message.channel.send("\:no_entry: I'm not playing anything yet, <@" + message.author.id + ">!");
 
                 // Call end event:
+                if(args[1] && /^\d+$/m.test(args[1])) {                
+                    if(parseInt(args[1]) <= queue[message.guild.id].urls.length) {
+                        queue[message.guild.id].urls.splice(0, parseInt(args[1]));
+                    } else {
+                        message.channel.send("\:no_entry: Number is out of range, <@" + message.author.id + ">!");
+                    }
+                }
                 queue[message.guild.id].dispatcher.end();
                 break;
             }
@@ -52,7 +59,17 @@ module.exports = {
                 delete queue[message.guild.id];
                 message.channel.send("\:firecracker: Cleared queue!");
                 break;
-            } 
+            }
+            case 'wrongsong': {
+                // Guards:
+                if(!queue[message.guild.id] || !queue[message.guild.id].urls) return message.channel.send("\:no_entry: Queue is empty, <@" + message.author.id + ">!");
+
+                // Remove last song:
+                ytdl.getBasicInfo(queue[message.guild.id].urls.pop()).then(data => {
+                    message.channel.send("\:open_mouth: Removed " + '"*' + data.title + '*"' + " from the queue, <@" + message.author.id + ">!");
+                });
+                break;
+            }
             case 'queue': {
                 // Guards:
                 if(!queue[message.guild.id] || !queue[message.guild.id].urls) return message.channel.send("\:no_entry: Queue is empty, <@" + message.author.id + ">!");
@@ -70,11 +87,11 @@ module.exports = {
                         await ytdl.getBasicInfo(queue[message.guild.id].urls[i], (err, data) => { // s
                             // Fill embed:
                             const length = (parseInt(data.length_seconds) / 60).toFixed(2) + " minutes";
-                            embed.addField((i+1) + ".", "Title: *" + data.title + "*\nDuration: *" + length + "*");
+                            embed.addField((!i ? "Up next:" : "-"), "Title: *" + data.title + "*\nDuration: *" + length + "*");
                             
                             // Show remaining songs:
                             const remaining = queue[message.guild.id].urls.length - maxSize;
-                            if(i >= maxSize-1) embed.addField("...", "and " + remaining + " more!");
+                            if(i >= maxSize) embed.addField("...", "and " + remaining + " more!");
                         });
                     }
                 })().then(() => message.channel.send(embed));
@@ -106,7 +123,7 @@ module.exports = {
                         const url = 'https://www.youtube.com/watch?v=' + response.data.items[0].id.videoId;
                         queue[message.guild.id].urls.push(url);
                         ytdl.getBasicInfo(url).then(data => {
-                            message.channel.send("\:ok_hand: Added: " + '"*' + data.title + '*"' + " based on your search query, <@" + message.author.id + ">!");
+                            message.channel.send("\:ok_hand: Added " + '"*' + data.title + '*"' + " based on your search query, <@" + message.author.id + ">!");
                         });
                     });
                 }
