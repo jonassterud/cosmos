206ff3cf1c7c316ad15decfee31869694b455fb5
jonassterud <jonas.sterud@gmail.com>
2020-04-26 21:20:06 +0200

    newmusic replacing (old) music

--- src/commands/music.js
+++ src/commands/music.js
@@ -1,127 +1,199 @@
 // Global variables
-let queue = [], playing = false, connection = "";
-const {google} = require('googleapis');
-const youtube = google.youtube({version: 'v3', auth: secret.youtube});
+let queue = {};
 
 // Command
 module.exports = {
     name: 'music',
     description: '\:musical_note: Listen to the audio from a YouTube video in your current voice channel!',
     args: true,
-    usage: '<YouTube URL, \'skip\' or \'queue\'>',
+    usage: '<YouTube video, playlist or query | skip [amount] | queue | pause | resume | reset>',
     execute(message, args) {
         // Variables:
         const voice = message.member.voiceChannel;
-        
-        // Check for errors:
+
+        // Check voice:
         if (!voice) return message.channel.send("\:no_entry: Please join a voicechannel before executing the command, <@" + message.author.id + ">!");
 
-        // Check arguments:
-        if (args[0] === 'skip') {
-            if(connection != "") {
-                connection.dispatcher.end();
-                return message.channel.send("\:cd: Skipping song...");
+        // Check argument:
+        switch(args[0].toLowerCase()) {
+            case 'pause': {
+                // Guards:
+                if(!queue[message.guild.id] || !queue[message.guild.id].dispatcher) return message.channel.send("\:no_entry: I'm not playing anything yet, <@" + message.author.id + ">!");
+                if(queue[message.guild.id].dispatcher.paused) return message.channel.send("\:question: I'm already paused, <@" + message.author.id + ">!");
+
+                // Pause:
+                message.channel.send("\:pause_button: Paused song, <@" + message.author.id + ">!");
+                queue[message.guild.id].dispatcher.pause(true);
+                break;
             }
-            return message.channel.send("No songs in queue " + message.author);
-        }
-        if(args[0] === 'queue') {
-            if (!queue.length) return message.channel.send("No songs in queue " + message.author);
+            case 'resume': {
+                // Guards:
+                if(!queue[message.guild.id] || !queue[message.guild.id].dispatcher) return message.channel.send("\:no_entry: I'm not playing anything yet, <@" + message.author.id + ">!");
+                if(!queue[message.guild.id].dispatcher.paused) return message.channel.send("\:question: I'm not paused, <@" + message.author.id + ">!");
 
-            let embed = new Discord.RichEmbed()
-                .setTitle("Song queue \:musical_note:")
-                .setColor('#ff0000')
-                .setTimestamp(new Date());
+                // Resume:
+                message.channel.send("\:arrow_forward: Resumed song, <@" + message.author.id + ">!");
+                queue[message.guild.id].dispatcher.resume();
+                break;
+            }
+            case 'skip': {
+                // Guards:
+                if(!queue[message.guild.id] || !queue[message.guild.id].connection) return message.channel.send("\:no_entry: I'm not playing anything yet, <@" + message.author.id + ">!");
 
-            const loop = async () => {
-                for (let i = 0; i < queue.length; i++) {
-                    let songUrl = queue[i];
-                    await ytdl.getInfo(songUrl).then(info => {
-                        if (i == 0) embed.addField("Current song:", " " + info.title);
-                        else embed.addField("Song number " + i + ":", " " + info.title);
-                        if (i == queue.length - 1) return message.channel.send(embed);
-                    }).catch(e => {
-                        console.log("Error getting song info.\nERROR:\n" + e);
+                // Skip:
+                if(args[1] && /^\d+$/m.test(args[1])) {                
+                    if(parseInt(args[1]) <= queue[message.guild.id].urls.length) {
+                        queue[message.guild.id].urls.splice(parseInt(args[1]) - 1);
+                        queue[message.guild.id].dispatcher.end();
+                    } else {
+                        message.channel.send("\:no_entry: Number is out of range, <@" + message.author.id + ">!");
+                    }
+                } else {
+                    queue[message.guild.id].dispatcher.end();
+                }
+                break;
+            }
+            case 'reset': {
+                // Guards:
+                if(!queue[message.guild.id]) return message.channel.send("\:no_entry: There is nothing to reset, <@" + message.author.id + ">!");
+                
+                // Reset:
+                queue[message.guild.id].urls = [];
+                queue[message.guild.id].dispatcher.end();
+                message.channel.send("\:firecracker: Cleared queue!");
+                break;
+            }
+            case 'wrongsong': {
+                // Guards:
+                if(!queue[message.guild.id] || !queue[message.guild.id].urls) return message.channel.send("\:no_entry: Queue is empty, <@" + message.author.id + ">!");
+
+                // Remove last song:
+                ytdl.getBasicInfo(queue[message.guild.id].urls.pop()).then(data => {
+                    message.channel.send("\:open_mouth: Removed " + '"*' + data.title + '*"' + " from the queue, <@" + message.author.id + ">!");
+                });
+                break;
+            }
+            case 'queue': {
+                // Guards:
+                if(!queue[message.guild.id] || !queue[message.guild.id].urls) return message.channel.send("\:no_entry: Queue is empty, <@" + message.author.id + ">!");
+
+                // Create embed:
+                let embed = new Discord.RichEmbed()
+                    .setTitle("Song queue \:musical_note:")
+                    .setColor('#ff0000')
+                    .setTimestamp(new Date());
+
+                // Loop trough songs:
+                const maxSize = 3;
+                (async function addItems() {
+                    for(let i=0; i<queue[message.guild.id].urls.length && i<maxSize; i++) {
+                        await ytdl.getBasicInfo(queue[message.guild.id].urls[i], (err, data) => { // s
+                            // Fill embed:
+                            const length = parseInt(data.length_seconds);
+                            embed.addField((!i ? "Currently playing:" : i + "."), "Title: *" + data.title + "*\nDuration: *" + Math.floor(length / 60) + " minutes and" + length % 60 + " seconds" + "*");
+                            
+                            // Show remaining songs:
+                            const remaining = queue[message.guild.id].urls.length - maxSize;
+                            if(i >= maxSize) embed.addField("...", "and " + remaining + " more!");
+                        });
+                    }
+                })().then(() => message.channel.send(embed));
+                break;
+            }
+            default: {
+                // Create queue:
+                if(!queue[message.guild.id]) {
+                    queue[message.guild.id] = {
+                        urls: [], 
+                        playing: false, 
+                        stream: undefined, 
+                        dispatcher: undefined,
+                        connection: undefined
+                    };
+                }
+
+                // Extract data from URL:
+                const regexResult = /www\.youtube\.com\/(?:watch\?v=([^&]+)|playlist\?)(?:&*list=([^&]+))*/.exec(args[0]);
+
+                // Add to queue based on URL type:
+                if(!regexResult) { // Search
+                    youtube.search.list({
+                        part: 'snippet',
+                        type: 'video',
+                        q: args.join(" "),
+                        key: secret.youtube
+                    }).then(response => {
+                        const url = 'https://www.youtube.com/watch?v=' + response.data.items[0].id.videoId;
+                        queue[message.guild.id].urls.push(url);
+                        ytdl.getBasicInfo(url).then(data => {
+                            message.channel.send("\:ok_hand: Added " + '"*' + data.title + '*"' + " based on your search query, <@" + message.author.id + ">!");
+                        });
                     });
                 }
-            }
-
-            return loop();
-        }
-
-        // Check for errors:
-        const re = /youtube.com\/watch\?v=[a-zA-Z\d\=\&\-\_]+$/g
-
-        if (!re.test(args[0])) {
-            const query = args.join(" ");
-            youtube.search.list({
-                part: 'snippet',
-                type: 'video',
-                order: 'relevance',
-                q: query,
-                maxResults: 10,
-                key: secret.youtube
-            }).then((searchRequest) => {
-                const result = searchRequest.data.items[0];
-                const youtubeUrl = ('https://www.youtube.com/watch?v=' + result.id.videoId);
-                queue.push(youtubeUrl);
-                ytdl.getInfo(youtubeUrl).then(info => message.channel.send("\:ok_hand: Added: " + '"' + info.title + '"' + " to the queue!")).catch();
-                tryPlay();
-            }).catch(e => console.error(e));
-            return;
-        }
-
-        // Add to queue:
-        queue.push(args[0]);
-        ytdl.getInfo(args[0]).then(info => message.channel.send("\:ok_hand: Added: " + '"' + info.title + '"' + " to the queue!")).catch();
-        
-        // Play:
-        function tryPlay() {
-            if (!playing) {
-                if (connection === '') {
-                    voice.join().then(conn => {
-                        connection = conn;
-                        play();
-                    }).catch();
-                } else {
-                    play();
+                else if(regexResult[2]) { // Playlist
+                    (function getItems(nextPageToken='') {
+                        youtube.playlistItems.list({
+                            playlistId: args[0].match(/list=([^&]+)/)[1],
+                            part: 'snippet',
+                            maxResults: 50,
+                            pageToken: nextPageToken
+                        }).then(response => {
+                            response.data.items.forEach(item => {
+                                queue[message.guild.id].urls.push("www.youtube.com/watch?v=" + item.snippet.resourceId.videoId);
+                            });
+                            if(response.data.nextPageToken) getItems(response.data.nextPageToken);
+                        });
+                    })();
+                    message.channel.send("\:ok_hand: Added songs from the playlist to the queue, <@" + message.author.id + ">!");
+                } else if(regexResult[1] && !regexResult[2]) { // Video
+                    queue[message.guild.id].urls.push(args[0]);
+                    message.channel.send("\:ok_hand: Added song to the queue, <@" + message.author.id + ">!");
                 }
+                
+                // Play:
+                if(!queue[message.guild.id].playing) play();
+                break;
             }
         }
-        tryPlay();
 
+        // Functions:
         function play() {
-            if (queue.length) {
-                // Change state:
-                playing = true;
+            // Join channel:
+            voice.join().then(connection => {
+                // Update queue variables:
+                queue[message.guild.id].connection = connection;
+                queue[message.guild.id].playing = true;
 
-                // Print title:
-                ytdl.getInfo(queue[0]).then(info => message.channel.send("\:musical_note: Now playing: " + info.title)).catch();
-
-                // Play song:
-                let stream = ytdl(queue[0], {
-                    quality: "highest",
+                // Download song:
+                queue[message.guild.id].stream = ytdl(queue[message.guild.id].urls[0], {
+                    quality: "highestaudio",
                     filter: "audioonly",
-                    highWaterMark: 1024 * 1024 * 10 /*10 Megabytes, default is 16kb*/
+                    highWaterMark: 1024 * 1024 * 30 // 30mb
                 });
-                let disp = connection.playStream(stream, {
-                    seek: 0,
-                    volume: 1,
+    
+                // Play stream:
+                queue[message.guild.id].dispatcher = connection.playStream(queue[message.guild.id].stream, {
+                    highWaterMark: 1,
+                    bitrate: 'auto'
                 });
-                disp.setBitrate('auto');
 
-                // Finished song event:
-                disp.on('end', () => {
-                    queue.shift();
-                    playing = false;
-                    tryPlay();
+                // Set volume:
+                queue[message.guild.id].dispatcher.setVolumeDecibels(-16);
+    
+                // End event:
+                queue[message.guild.id].dispatcher.on('end', () => {
+                    queue[message.guild.id].urls.shift();
+                    if(queue[message.guild.id].urls.length) {
+                        message.channel.send("\:ok_hand: Playing next song from queue..");
+                        play();
+                    } else {
+                        voice.leave();
+                        connection.disconnect();
+                        delete queue[message.guild.id];
+                        message.channel.send("\:weary: Session over!");
+                    }
                 });
-            } else {
-                playing = false;
-                connection = "";
-                message.channel.send("\:frowning: Session over. No more songs in queue!");
-                voice.leave();
-                return;
-            }
+            });
         }
     }
 };
