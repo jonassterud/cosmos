89891f5a63e79017f1846bede9669e65380a9989
jonassterud <jonas.sterud@gmail.com>
2020-04-29 16:50:35 +0200

    Fixed raffle + now using a reaction collector

--- src/commands/raffle.js
+++ src/commands/raffle.js
@@ -5,71 +5,51 @@ module.exports = {
     args: true,
     usage: '<hh:mm:ss> <item>',
     execute (message, args) {
-        message.channel.send('Under construction..');
-    /*
-        const guild = message.guild.id;
-        const auth = message.author.id;
-        const re = /^(\d\d:\d\d:\d\d)$/;
-        if(!re.test(args[0]) || args.length <= 1) return;
-        const nums = args[0].split(":");
-        const creator = message.author;
-        let creditGiveaway = false;
-        const decideType = /^(\d)+/;
-        let data = JSON.parse(fs.readFileSync('./data.json'));
-        let testString = "";
+        // Guards:
+        if(!/^\d\d:\d\d:\d\d$/m.test(args[0]) || args.length <= 1) return;
+        if(!args[1]) return;
 
-        args.slice(1).forEach(inner => testString += inner);
-        creditGiveaway = decideType.test(testString);
-        if(creditGiveaway && Number(args[1]) > data[message.guild.id]['users'][message.author.id]['credits']) {
-            return message.channel.send("\:moneybag: You don't have enough credits, <@" + message.author.id + ">!");
-        }
+        // Create raffle:
+        const timeArray = args[0].split(':');
+        let timeText = ''; // Format text nicely: plural numbers, comma/and, etc. -> bad solution?
+        if(parseInt(timeArray[0])) timeText += parseInt(timeArray[0]) + (parseInt(timeArray[0]) > 1 ? ' hours' : ' hour') + (!parseInt(timeArray[2]) ? (!parseInt(timeArray[1]) ? '.' : ' and ') : ', ');
+        if(parseInt(timeArray[1])) timeText += parseInt(timeArray[1]) + (parseInt(timeArray[1]) > 1 ? ' minutes' : ' minute') + (!parseInt(timeArray[2]) ? '.' : ' and ');
+        if(parseInt(timeArray[2])) timeText += parseInt(timeArray[2]) + (parseInt(timeArray[2]) > 1 ? ' seconds.' : ' second.');
+        const item = args.splice(1).join(' ');
+        const entryEmbed = new Discord.MessageEmbed()
+            .setTitle('\:tada: Raffle - ' + item)
+            .setDescription(
+                '<@' + message.author.id + '> is giving away ' + item + '!\n' +
+                'To join, just react with **any** emoji below.\n' +
+                'The giveaway will end in:\n`' + timeText + '`\n' +
+                'Good luck! \:+1:'
+            )
+            .setColor('#ff0000')
+            .setTimestamp(new Date());
 
-        let sumSeconds = 0;
-        nums.forEach((number,i) => {
-            if(i == 0) sumSeconds += Number(number) * 3600;
-            if(i == 1) sumSeconds += Number(number) * 60;
-            if(i == 2) sumSeconds += Number(number);
+        message.channel.send(entryEmbed).then(e => {
+            // Create collector:
+            const filter = (_, user) => user.id != message.author.id;
+            const waitTime = (1000 * 60 * 60 * timeArray[0]) + (1000 * 60 * timeArray[1]) + (1000 * timeArray[2]);
+            const collector = e.createReactionCollector(filter, {time: waitTime});
+            let contestants = [];
+
+            // Collector events:
+            collector.on('collect', (_, user) => {
+                const id = user.id;
+                if(!contestants.includes(id)) contestants.push(id);
+            });
+
+            collector.on('end', () => {
+                const winner = contestants[Math.floor(Math.random() * contestants.length)];
+                const endEmbed = new Discord.MessageEmbed()
+                    .setTitle('\:tada: The raffle is over!')
+                    .setDescription(contestants.length ? '<@' + winner + '> won ' + item + '!\nContact <@' + message.author.id + '> for your prize!' : 'But nobody entered in time!')
+                    .setColor('#ff0000')
+                    .setTimestamp(new Date());
+
+                message.channel.send(endEmbed);
+            });
         });
-
-        //Choose winner:
-        data[guild]['users'][auth]['credits'] -= parseFloat(args[1]);
-        fs.writeFileSync('./data.json', JSON.stringify(data));
-        const filter = reaction => { return ['💎'].includes(reaction.emoji.name); }
-        let embed = new Discord.MessageEmbed();
-        embed.setTitle(message.author.username + " started a raffle!");
-        embed.setThumbnail(message.author.avatarURL);
-        embed.setColor('#ff0000');
-        embed.addField('Prize:', args.slice(1).join(" ")+"\n React with a 💎 to enter the raffle");
-
-        message.channel.send(embed).then( message => {
-            message.react("💎");
-            console.log(sumSeconds);
-            message.awaitReactions(filter,{max:50,time:sumSeconds*1000}).then(collection => {
-                //There is probably a much better way to do this, will look into it later
-                let keyVals = [];
-                for(const [key, value] of collection.first().users.entries()){
-                    if(key == creator.id || key == client.user.id) continue;
-                    keyVals.push(key);
-                }
-
-                if(keyVals.length < 1){
-                    data[guild]['users'][auth]['credits'] += parseFloat(args[1]);
-                    fs.writeFileSync('./data.json', JSON.stringify(data));
-                    return message.channel.send("Nobody joined the raffle in time")
-                };
-
-                let winner = keyVals[Math.floor(Math.random() * (keyVals.length))];
-                client.fetchUser(winner).then(nm => {
-                    console.log(keyVals);
-                    message.channel.send("💎 Hey "+ creator + ", "+nm+" won your raffle").then(msg => {
-                        console.log(creditGiveaway);
-                        if(!creditGiveaway) return;
-                        data[guild]['users'][nm.id]['credits'] += parseFloat(args[1]);
-                        return fs.writeFileSync('./data.json', JSON.stringify(data));
-                    }).catch();
-                }).catch();
-            }).catch();
-        }).catch();
-        */
     }
 };
