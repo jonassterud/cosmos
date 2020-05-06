cc9f130be634848d076a0cc680fda91640dc2e95
Sebastian Refsnes <sebastian.refsnes@gmail.com>
2020-05-06 04:40:43 +0200

    purge now deletes the previous n elements including the purge query and the response

--- src/commands/administration/purge.js
+++ src/commands/administration/purge.js
@@ -9,7 +9,12 @@ module.exports = {
         if(!/^100$|^[1-9][0-9]$|^[0-9]$/m.test(args[0])) return message.channel.send(`\:question: Invalid amount. Make sure the amount is in the range from 1 to 100, <@${message.author.id}>!`);
 
         // Purge:
-        await message.channel.bulkDelete(parseInt(args[0]));
-        return message.channel.send(`\:boom: Deleted ${parseInt(args[0])} message${(parseInt(args[0]) > 1) ? 's' : ''}, <@${message.author.id}>!`);
+        await message.channel.bulkDelete(parseInt(args[0])+1);
+        message.channel.send(`\:boom: Deleted ${parseInt(args[0])} message${(parseInt(args[0]) > 1) ? 's' : ''}, <@${message.author.id}>!`).then((msg) => {
+            msg.delete({timeout:5000,reason:""});
+        }).catch((err) => {
+            console.error(err);
+        });
+        return;
     }
 };
