c5e1754629b14f052d52aa5aa19cc81068f5a973
jonassterud <jonas.sterud@gmail.com>
2020-05-06 10:55:37 +0200

    Removed then and better error handling

--- src/commands/administration/purge.js
+++ src/commands/administration/purge.js
@@ -9,12 +9,13 @@ module.exports = {
         if(!/^100$|^[1-9][0-9]$|^[0-9]$/m.test(args[0])) return message.channel.send(`\:question: Invalid amount. Make sure the amount is in the range from 1 to 100, <@${message.author.id}>!`);
 
         // Purge:
-        await message.channel.bulkDelete(parseInt(args[0])+1);
-        message.channel.send(`\:boom: Deleted ${parseInt(args[0])} message${(parseInt(args[0]) > 1) ? 's' : ''}, <@${message.author.id}>!`).then((msg) => {
-            msg.delete({timeout:5000,reason:""});
-        }).catch((err) => {
-            console.error(err);
-        });
-        return;
+        try {
+            await message.channel.bulkDelete(parseInt(args[0]) + 1);
+            const sentMessage = await message.channel.send(`\:boom: Deleted ${parseInt(args[0])} message${(parseInt(args[0]) > 1) ? 's' : ''}, <@${message.author.id}>!`);
+            return sentMessage.delete({timeout: 5000});
+        } catch(error) {
+            client.logger.error(error);
+            return message.channel.send(`\:no_entry: Something went wrong, <@${message.author.id}>!`);
+        }
     }
 };
