28cb492b3b99c238c8f44a47fc9cb06f42cb228d
jonas-laptop <jonas.sterud@gmail.com>
2020-05-27 23:44:54 +0200

    Changed logging

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,4 +1,4 @@
 module.exports = async client => {
-    client.logger.ready('Online!');
+    client.logger.ready(`Online in ${client.guilds.cache.size} server${client.guilds.cache.size > 1 ? 's' : ''}`);
     client.user.setActivity(`${config.prefix}help`, {type: 'LISTENING'});
 };
--- src/modules/Loader.js
+++ src/modules/Loader.js
@@ -18,8 +18,10 @@ exports.registerCommands = async client => {
 
     // Loop trough directories:
     for(const dir of dirs) {
-        client.logger.log(`Loading ${dir} commands`);
         const files = fs.readdirSync(`./commands/${dir}`);
+
+        // Loop trough directories:
+        client.logger.log(`Loading ${files.length} ${dir} command${files.length > 1 ? 's' : ''}`);
         for(const file of files) {
             // Add subfolders:
             if(fs.statSync(`./commands/${dir}/${file}`).isDirectory()) {
@@ -34,22 +36,21 @@ exports.registerCommands = async client => {
             registeredCommands.push(name);
         }
     }
-    client.logger.log(`Loaded: [${registeredCommands.join(' ')}]`);
 };
 
 // Load events into client:
 exports.registerEvents = async client => {
     const eventFiles = fs.readdirSync('./events/');
-    client.logger.log(`Loading ${eventFiles.length} events`);
-
     const registeredEvents = [];
+    
+    // Add events to client:
+    client.logger.log(`Loading ${eventFiles.length} event${eventFiles.length > 1 ? 's' : ''}`);
     eventFiles.forEach(file => {
         const eventName = file.split('.')[0];
         const evt = require(`../events/${file}`);
         client.on(eventName, evt.bind(null, client));
         registeredEvents.push(eventName);
     });
-    client.logger.log(`Loaded: [${registeredEvents.join(' ')}]`);
 };
 
 // Log Discord status:
