ca9b14a9faf1e293c07b037e62c517f2fac63dae
jonassterud <57707537+jonassterud@users.noreply.github.com>
2020-10-08 20:40:14 +0200

    Ping

--- src/commands/other/hello.js
+++ src/commands/other/hello.js
@@ -1,9 +1,9 @@
 module.exports = {
     name: 'hello',
-    description: '\:wave: Hello!',
+    description: '\:wave: Use this command to check the latency of the bot!',
     args: false,
     usage: '',
     async execute(message) {
-        return message.channel.send(`\:wave: Hello, <@${message.author.id}>!`);
+        return message.channel.send(`\:wave: Hello, <@${message.author.id}>! I have a latency of ${Date.now() - message.createdTimestamp}ms.`);
     }
 };
