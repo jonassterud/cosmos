936c6fed538c457b3e6663af7a15123539416f96
Sebastian Refsnes <sebastian.refsnes@gmail.com>
2021-01-26 13:02:39 +0100

    User can now choose category in trivia

--- src/commands/games/trivia.js
+++ src/commands/games/trivia.js
@@ -12,7 +12,7 @@ function shuffle(array) {
     }
     return array;
 }
-const triviaCategories= `{"trivia_categories":[{"id":9,"name":"General Knowledge"},{"id":10,"name":"Entertainment: Books"},{"id":11,"name":"Entertainment: Film"},{"id":12,"name":"Entertainment: Music"},{"id":13,"name":"Entertainment: Musicals & Theatres"},{"id":14,"name":"Entertainment: Television"},{"id":15,"name":"Entertainment: Video Games"},{"id":16,"name":"Entertainment: Board Games"},{"id":17,"name":"Science & Nature"},{"id":18,"name":"Science: Computers"},{"id":19,"name":"Science: Mathematics"},{"id":20,"name":"Mythology"},{"id":21,"name":"Sports"},{"id":22,"name":"Geography"},{"id":23,"name":"History"},{"id":24,"name":"Politics"},{"id":25,"name":"Art"},{"id":26,"name":"Celebrities"},{"id":27,"name":"Animals"},{"id":28,"name":"Vehicles"},{"id":29,"name":"Entertainment: Comics"},{"id":30,"name":"Science: Gadgets"},{"id":31,"name":"Entertainment: Japanese Anime & Manga"},{"id":32,"name":"Entertainment: Cartoon & Animations"}]}`;
+const triviaCategories= JSON.parse(`{"trivia_categories":[{"id":9,"name":"General Knowledge"},{"id":10,"name":"Entertainment: Books"},{"id":11,"name":"Entertainment: Film"},{"id":12,"name":"Entertainment: Music"},{"id":13,"name":"Entertainment: Musicals & Theatres"},{"id":14,"name":"Entertainment: Television"},{"id":15,"name":"Entertainment: Video Games"},{"id":16,"name":"Entertainment: Board Games"},{"id":17,"name":"Science & Nature"},{"id":18,"name":"Science: Computers"},{"id":19,"name":"Science: Mathematics"},{"id":20,"name":"Mythology"},{"id":21,"name":"Sports"},{"id":22,"name":"Geography"},{"id":23,"name":"History"},{"id":24,"name":"Politics"},{"id":25,"name":"Art"},{"id":26,"name":"Celebrities"},{"id":27,"name":"Animals"},{"id":28,"name":"Vehicles"},{"id":29,"name":"Entertainment: Comics"},{"id":30,"name":"Science: Gadgets"},{"id":31,"name":"Entertainment: Japanese Anime & Manga"},{"id":32,"name":"Entertainment: Cartoon & Animations"}]}`);
 
 // Command
 module.exports = {
@@ -21,15 +21,18 @@ module.exports = {
     args: false,
     usage: '[optional category]',
     async execute(message, args) {
-        let arg = '';
+        let cat = '';
         if(args != undefined && args.length != 0){
-            if(triviaCategories.trivia_categories.find(obj => () => {
-                if(obj.name.split(' ').includes(args[0])) return arg = obj.id;
-                })){
-                arg != '' ? arg = '&category='+arg : arg = '';
+            triviaCategories.trivia_categories.find(obj => {
+            if(obj.name.toLowerCase().split(' ').includes(args[0].toLowerCase())){
+                cat = obj.id;
+                return true;
             }
+            return false;
+            });
+            cat != '' ? cat = '&category='+cat : cat = '';
         }
-        https.get('https://opentdb.com/api.php?amount=1&difficulty=medium&type=multiple'+arg, response => {
+        https.get('https://opentdb.com/api.php?amount=1&difficulty=medium&type=multiple'+cat, response => {
             // Guards:
             let str = '';
             response.on('data', chunk => {
