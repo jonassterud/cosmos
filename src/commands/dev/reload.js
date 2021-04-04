f34ee1f49c22473dc4f679429e8fec0d4a234d19
Sebastian Refsnes <sebastian.refsnes@gmail.com>
2021-04-04 21:22:02 +0200

    Fixed reload all not working

--- src/commands/dev/reload.js
+++ src/commands/dev/reload.js
@@ -1,20 +1,40 @@
 module.exports = {
-    name: "reload",
-    description: ":arrows_counterclockwise: Use this command to reload a given command!",
+    name: 'reload',
+    description: ':arrows_counterclockwise: Use this command to reload a given command!',
     args: false,
-    usage: "<command>",
+    usage: '<command>',
     async execute(message, args) {
         //not for everyone
-        if (!["181250087442317312", "305074536825749504"].includes(message.author.id)) {
+        if (!['181250087442317312', '305074536825749504'].includes(message.author.id)) {
             return message.channel.send(`\:no_entry: Permission denied!`);
         }
         const client = message.client;
         if (args == undefined || args.length == 0) {
-            await client.loader.registerCommands(client);
+            const dirs = fs.readdirSync('./commands/').filter((file) => fs.statSync(`./commands/${file}`).isDirectory());
+            for (const dir of dirs) {
+                const files = fs.readdirSync(`./commands/${dir}`);
+                for (const file of files) {
+                    if (fs.statSync(`./commands/${dir}/${file}`).isDirectory()) {
+                        dirs.push(`${dir}/${file}`);
+                        continue;
+                    }
+                    if (client.commands.has(file.split('.').shift())) {
+                        try {
+                            delete require.cache[require.resolve(`${global.rootPath}/commands/${dir}/${file}`)];
+                            const newProperties = require(`${global.rootPath}/commands/${dir}/${file}`);
+                            client.commands.set(newProperties.name, newProperties);
+                        } catch (e) {
+                            client.logger.error(`Something went wrong trying to reload a file via the reload command!`);
+                            client.logger.error(e);
+                            return message.channel.send(`\:no_entry: Something went wrong`);
+                        }
+                    }
+                }
+            }
             return message.channel.send(`\:arrows_counterclockwise: Reloaded all commands!`);
         }
         const cmdName = args[0];
-        const dirs = fs.readdirSync("./commands/").filter((file) => fs.statSync(`./commands/${file}`).isDirectory());
+        const dirs = fs.readdirSync('./commands/').filter((file) => fs.statSync(`./commands/${file}`).isDirectory());
         for (const dir of dirs) {
             const files = fs.readdirSync(`./commands/${dir}`);
             for (const file of files) {
@@ -22,10 +42,11 @@ module.exports = {
                     dirs.push(`${dir}/${file}`);
                     continue;
                 }
-                if (file.split(".").shift() == cmdName) {
+                if (file.split('.').shift() == cmdName) {
                     try {
                         delete require.cache[require.resolve(`${global.rootPath}/commands/${dir}/${file}`)];
                         const newProperties = require(`${global.rootPath}/commands/${dir}/${file}`);
+                        console.log(newProperties.name);
                         client.commands.set(newProperties.name, newProperties);
                         return message.channel.send(`\:arrows_counterclockwise: Command: ${cmdName} reloaded!`);
                     } catch (e) {
