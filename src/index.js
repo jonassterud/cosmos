682beb8ff6623347845b77e6a3179a1f687ff243
Jonas Mathisrud Sterud <jonas.sterud@gmail.com>
2021-05-26 09:35:45 +0000

    devcontainer configuration and eslint auto fix

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
 // Package(s):
-require("dotenv").config();
-global.config = require("./config.json"); // Secret data
-global.fs = require("fs"); // Node.js package - file system
-global.Discord = require("discord.js"); // Node.js package - Discord API
-global.http = require("http"); // Node.js packae - Request data from APIs
-global.https = require("https"); // Node.js package - Request data from APIs
-global.ytdl = require("ytdl-core"); // Node.js package - Downloads YouTube videos
-global.youtube = require("googleapis").google.youtube({ version: "v3", auth: process.env.YOUTUBE }); // Youtube API
-global.decks = require("cards").decks; // Node.js package - Playing cards API
-global.moment = require("moment"); // Node.js package - Dates and time
-global.finance = require("alphavantage")({ key: process.env.ALPHAVANTAGE }); // Node.js package - Wrapper for Alpha Vantage API
+require('dotenv').config();
+global.config = require('./config.json'); // Secret data
+global.fs = require('fs'); // Node.js package - file system
+global.Discord = require('discord.js'); // Node.js package - Discord API
+global.http = require('http'); // Node.js packae - Request data from APIs
+global.https = require('https'); // Node.js package - Request data from APIs
+global.ytdl = require('ytdl-core'); // Node.js package - Downloads YouTube videos
+global.youtube = require('googleapis').google.youtube({ version: 'v3', auth: process.env.YOUTUBE }); // Youtube API
+global.decks = require('cards').decks; // Node.js package - Playing cards API
+global.moment = require('moment'); // Node.js package - Dates and time
+if(process.env.ALPHAVANTAGE) {
+    global.finance = require('alphavantage')({ key: process.env.ALPHAVANTAGE }); // Node.js package - Wrapper for Alpha Vantage API
+}
 global.rootPath = __dirname;
 
 // Global(s):
 global.queue = {};
-global.accounts = fs.existsSync("./backups/accounts.json") ? JSON.parse(fs.readFileSync("./backups/accounts.json")) : {};
+global.accounts = fs.existsSync('./backups/accounts.json') ? JSON.parse(fs.readFileSync('./backups/accounts.json')) : {};
 
 // Backup loop:
 setInterval(() => {
-    if (!fs.existsSync("./backups")) fs.mkdirSync("./backups");
-    fs.writeFileSync("./backups/accounts.json", JSON.stringify(accounts, null, 4));
+    if(!fs.existsSync('./backups')) fs.mkdirSync('./backups');
+    fs.writeFileSync('./backups/accounts.json', JSON.stringify(accounts, null, 4));
 }, 1000 * 60 * 5);
 
 // Create bot:
@@ -27,7 +29,7 @@ client = new Discord.Client();
 
 // Create commands:
 client.commands = new Discord.Collection();
-client.loader = require("./modules/Loader");
+client.loader = require('./modules/Loader');
 
 (async () => {
     const loader = client.loader;
